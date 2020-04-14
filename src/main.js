3863829c9f8b9a63741195e6c8cadd28dfa8ff9c
Mikah Mathews <mikah@outofband.org>
2020-04-14 14:57:19 -0700

    Add updated javascript, trying to test css styles

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './triangle.js';
 
+
+
+$(document).ready(function(){
+  $("#user-input").submit(function(event){
+    event.preventDefault();
+    $("#result-scalene").show();
+    $("#result-equil").show();
+    $("#result-Isosceles").show();
+  });
+});
